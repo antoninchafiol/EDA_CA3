75443eeedf244cc384d197b05e7748441300fac1
Antonin <antonin.chafiol@gmail.com>
2024-11-21 16:05:19 +0000

    [ADD] - Loading Connecitin to API

--- CA3.Client/Program.cs
+++ CA3.Client/Program.cs
@@ -3,8 +3,5 @@ using static CA3.Client.Pages.Drug;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.fda.gov/") });
-builder.Services.AddScoped<OpenFdaService>();
-
 
 await builder.Build().RunAsync();
