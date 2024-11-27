9b898ee17abed97e9813509b4e201fa5bcaaf33f
Antonin <antonin.chafiol@gmail.com>
2024-11-27 11:38:49 +0000

    [UPDT] - Refactor to Mudblazor

--- CA3.Client/Program.cs
+++ CA3.Client/Program.cs
@@ -1,10 +1,12 @@
 using CA3.Client.Pages;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<DrugService>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
